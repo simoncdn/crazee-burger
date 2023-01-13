ab5b763f71c31f3a4b5df8f9658682d5834db3a2
Cardona simon <simon.cdn@icloud.com>
2023-01-13 23:08:47 +0100

    🐛 F02 : Fix npm build bug

---
+++ src/components/Pages/error/ErrorPage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Error() {
+
+  return (
+    <div>
+        <h1>ErrorPage</h1>
+        
+        <Link to={"/"}>
+          <button>Retourner à la page d'accueil</button>
+        </Link>
+
+    </div>
+  )
+}
