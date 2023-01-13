e5c4589b90c536d5fef8c2b264394eddbd346409
Cardona simon <simon.cdn@icloud.com>
2023-01-13 16:04:41 +0100

    ✨ F01: LOGIN > Create LoginForm Component

---
+++ src/Components/LoginForm/LoginForm.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { useState } from 'react'
+
+export default function LoginForm() {
+    const [username, setUsername] = useState("")
+
+    const handleChange = (event) => {
+      setUsername(event.target.value)
+    }
+  
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      alert(`Bonjour + ${username}`);
+      setUsername("")
+    }
+  return (
+    <div className='LoginForm-container'>
+
+        <div className="LoginForm-title">
+            <h2>Bienvenue chez nous !</h2>
+        </div>
+
+        <br />
+
+        <div className="LoginForm-connect">
+            <h3>Connectez vous</h3>
+        </div>
+
+        <div className="LoginForm">
+            <form onSubmit={handleSubmit}>
+
+                <label htmlFor="name"></label>
+                <input type="text" 
+                id="name" 
+                placeholder='Entrez votre prenom'
+                value={username}
+                onChange={handleChange}
+                required
+                />
+
+                <label htmlFor="btnConnexion"></label>
+                <button id='btnConnexion'>Accedez a votre espace</button>
+            </form>
+        </div>
+    </div>
+  )
+}
