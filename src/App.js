ce836763189fe496e8ff56768e05db1a881ced60
Cardona simon <simon.cdn@icloud.com>
2023-01-13 21:43:47 +0100

    ✨ F02 : APP > Create routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
-import LoginPage from './Components/Pages/LoginPage';
+import LoginPage from './components/Pages/LoginPage';
+import {Routes, Route} from 'react-router-dom';
+import Error from './components/Pages/Error/Error'
+import OrderPage from './components/Pages/OrderPage/OrderPage';
 
 function App() {
   return (
     <div className="App">
-      <LoginPage />
+      <Routes>
+        <Route path='/' element={<LoginPage />} />
+        <Route path='/order/:username' element={<OrderPage />} />
+        <Route path='/*' element={<Error />} />
+      </Routes>
     </div>
   );
 }
