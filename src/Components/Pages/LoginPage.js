3e49a4c96013b221b4b1826057777c28503df983
Cardona simon <simon.cdn@icloud.com>
2023-01-13 17:24:04 +0100

    ✏️ F01 : LOGINFORM > LoginPage > Fix Typo

--- src/Components/Pages/LoginPage.js
+++ src/Components/Pages/LoginPage.js
@@ -3,7 +3,7 @@ import LoginForm from './LoginForm/LoginForm'
 
 export default function LoginPage() {
   return (
-    <div className='LoginPage-container'>
+    <div className='LoginPageContainer'>
         <LoginForm />
     </div>
   )
