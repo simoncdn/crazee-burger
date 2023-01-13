60aa8fadb4949ffe045cb303d248df53eb6e0932
Cardona simon <simon.cdn@icloud.com>
2023-01-13 15:53:04 +0100

    🔥 F01: Delete code in app.js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,10 @@
-import {useState} from "react";
 import './App.css';
+import LoginForm from "./Components/LoginForm/LoginForm";
 
 function App() {
-
-  const [name, setName] = useState("")
-
-  const handleChange = (event) => {
-    setName(event.target.value)
-  }
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    alert(`Bonjour + ${name}`);
-    setName("")
-  }
-
   return (
     <div className="App">
-      <h2>Bienvenue chez nous !</h2>
-      <br />
-      <h3>Connectez vous</h3>
-
-      <form onSubmit={handleSubmit}>
-
-        <label htmlFor="name"></label>
-        <input type="text" 
-        id="name" 
-        placeholder='Entrez votre prenom'
-        value={name} 
-        onChange={handleChange}
-        required
-        />
-
-        <label htmlFor="btnConnexion"></label>
-        <button id='btnConnexion'
-        >Accedez a votre espace</button>
-      </form>
+      <LoginForm />
     </div>
   );
 }
