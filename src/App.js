06b8f7b3a56d033e773018789bda5bd6292d83a3
Cardona simon <simon.cdn@icloud.com>
2023-01-13 22:17:14 +0100

    ✏️ F02 : APP > Fix typo (Path for error route)

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<LoginPage />} />
         <Route path='/order/:username' element={<OrderPage />} />
-        <Route path='/*' element={<ErrorPage />} />
+        <Route path='*' element={<ErrorPage />} />
       </Routes>
     </div>
   );
