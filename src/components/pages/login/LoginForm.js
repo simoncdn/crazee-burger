318ffe6d41f09de8b9e85d25f84e3f0d04208b80
Cardona simon <simon.cdn@icloud.com>
2023-01-14 03:22:36 +0100

    💄 F03 : Login > Styling Done

--- src/components/pages/login/LoginForm.js
+++ src/components/pages/login/LoginForm.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import styled from "styled-components"
 
 export default function LoginForm() {
     const [username, setUsername] = useState("")
@@ -15,23 +16,76 @@ export default function LoginForm() {
       navigate(`order/${username}`)
       setUsername("")
     }
+
   return (
-    <div>
-        <form action='submit'
-          onSubmit={handleSubmit}>
-        <h2>Bienvenue chez nous !</h2>
-        <br />
-        <h3>Connectez-vous</h3>
+    <LoginFormStyled action='submit'
+      onSubmit={handleSubmit}>
+    
+      <h1>Bienvenue chez nous !</h1>
+      <br />
+      <h2>Connectez-vous</h2>
 
-        <input type="text" 
-          placeholder='Entrez votre prénom...'
-          value={username}
-          onChange={handleChange}
-          required
-        />
+      <input type="text" 
+        placeholder='Entrez votre prénom'
+        value={username}
+        onChange={handleChange}
+        required
+      />
 
-        <button>Accéder à votre espace</button>
-        </form>
-    </div>
+      <button>Accéder à mon espace </button>
+
+    </LoginFormStyled>
   )
 }
+
+const LoginFormStyled = styled.form `
+  z-index: 10;
+  positon: relative;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  width: 400px;
+  h1{
+    color: white;
+    font-family: 'Amatic SC';
+    font-size: 44px;
+    font-weight: 700;
+    padding: 30px 50px;
+    border-bottom: 3px solid orange;
+  }
+
+  h2{
+    color: white;
+    font-family: 'Amatic SC';
+    font-size: 36px;
+    font-weight: 400;
+    margin: 15px;
+  }
+
+  input{
+    border-radius: 5px;
+    width: 100%;
+    height: 55px;
+    padding: 15px 55px;
+    margin-bottom: 15px;
+    font-family: 'Open Sans';
+    ::placeholder{
+      color: light-grey;
+      font-family: 'Open Sans';
+      opacity: 0.5;
+    }
+  }
+
+  button{
+    width: 100%;
+    height: 55px;
+    color: orange;
+    border: 1px solid orange;
+    border-radius: 5px;
+    padding: 15px 55px;
+    font-family: 'Open Sans';
+    font-weight: 700;
+    font-size: 14px;
+  }
+
+`
--- src/components/pages/login/LoginPage.js
+++ src/components/pages/login/LoginPage.js
@@ -1,10 +1,33 @@
 import React from 'react'
 import LoginForm from './LoginForm'
+import styled from 'styled-components'
+import Logo from '../../logo/Logo'
 
 export default function LoginPage() {
   return (
-    <div>
-        <LoginForm />
-    </div>
+    <LoginPageStyled>
+      <Logo />
+      <LoginForm />
+    </LoginPageStyled>
   )
 }
+
+const LoginPageStyled = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+
+  ::before{
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 100vh;
+    background-image: url("./images/burger-background.jpg");
+    background-size: cover;
+    filter: brightness(40%);
+  }
+`
