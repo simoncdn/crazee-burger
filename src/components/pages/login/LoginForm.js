4e27334d2e97f2b4116a0e27e3307115824e4a48
Cardona simon <simon.cdn@icloud.com>
2023-01-14 07:10:01 +0100

    ♻️ F03 : LOGINFORM > Refacto

--- src/components/pages/login/LoginForm.js
+++ src/components/pages/login/LoginForm.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import styled, { ThemeConsumer } from "styled-components"
+import styled from "styled-components"
 import { theme } from '../../../theme'
-import { BsPersonCircle } from 'react-icons/bs'
 import { IoChevronForward } from 'react-icons/io5'
+import { BsPersonCircle } from 'react-icons/bs'
+import TextInput from '../../reusable-ui/TextInput'
+import PrimaryButton from '../../reusable-ui/PrimaryButton'
 
 export default function LoginForm() {
     const [username, setUsername] = useState("")
     const navigate = useNavigate();
-    
+
     const handleChange = (event) => {
       setUsername(event.target.value)
     }
-    
+
     const handleSubmit = (event) => {
       event.preventDefault();
       navigate(`order/${username}`)
@@ -28,22 +30,19 @@ export default function LoginForm() {
       <hr />
       <h2>Connectez-vous</h2>
 
-      <div className="input-with-icon">
-
-        <BsPersonCircle className='icon'/>
-
-        <input type="text" 
-          placeholder='Entrez votre prénom'
-          value={username}
-          onChange={handleChange}
-          required
-        />
-      </div>
+      <TextInput 
+      value={username} 
+      onChange={handleChange} 
+      placeholder={'Entrez votre prénom'}
+      type={'text'}
+      required
+      Icon={<BsPersonCircle className='icon'/>}
+      />
       
-      <button className='button-with-icon'>
-        <span>Accéder à mon espace </span>
-        <IoChevronForward className='icon'/>
-      </button>
+      <PrimaryButton 
+      label={"Accéder à mon espace"}
+      Icon={<IoChevronForward className='icon'/>}
+      />
 
     </LoginFormStyled>
   )
@@ -77,81 +76,12 @@ const LoginFormStyled = styled.form `
     border: 1.5px solid #f56a2c;
     margin-bottom: 30px;
   }
-
-  .input-with-icon{
-    display: flex;
-    align-items: center;
-    width: 100%;
-    height: 55px;
-    background-color: ${theme.colors.white};
-    border-radius: ${theme.borderRadius.round};
-    padding: 18px 24px;
-    margin: 18px 0;
-
-    .icon{
-      color: ${theme.colors.greySemiDark};
-      font-size: ${theme.fonts};
-      margin-right: 8px;
-    }
-
-    input{
-      font-size: ${theme.fonts.P0};
-      color: ${theme.colors.dark};
-      border: none;
-      width: 100%;
-
-      ::placeholder{
-        background: ${theme.colors.white};
-        color: ${theme.colors.greyLight};
-      }
-    }
-  }
-
-  .button-with-icon{
-    width: 100%;
-    display: inline-flex;
-    justify-content: center;
-    align-items: center;
-    border-radius: 5px;
-    position: relative;
-    white-space: nowrap;
-    text-decoration: none;
-    line-height: 1;
-    
-    padding: 18px 24px;
-    border-radius: ${theme.borderRadius.round};
-    font-size: ${theme.fonts.P0};
-    font-weight: ${theme.weights.heavy};
-    color: ${theme.colors.white};
-    background-color: ${theme.colors.primary_burger};
-    border: 1px solid ${theme.colors.primary_burger};
-    cursor: pointer;
-    
-    &:hover:not(:disabled){
-      background-color: ${theme.colors.white};
-      color: ${theme.colors.primary_burger};
-      border: 1px solid ${theme.colors.primary_burger};
-      transition: all 200ms ease-out;
-    }
-
-    &:active{
-      color: ${theme.colors.white};
-      background-color: ${theme.colors.primary_burger};
-      border: 1px solid ${theme.colors.primary_burger};
-    }
-
-    &:disabled{
-      opacity: 0.6;
-      cursor: not-allowed;
-    }
-
-    .icon{
+  
+  .icon{
       display: flex;
       justify-content: center;
       align-items: center;
       font-size: ${theme.fonts.P0};
       margin-left: 10px;
     }
-  }
-
 `
