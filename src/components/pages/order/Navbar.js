5a133aa8e673a3440ce572a63affa6f5e2a42b1c
Cardona simon <simon.cdn@icloud.com>
2023-01-23 14:19:31 +0100

    ✨ F04 : Add Circle Icon

--- src/components/pages/order/Navbar.js
+++ src/components/pages/order/Navbar.js
@@ -2,19 +2,40 @@ import React from 'react'
 import Logo from '../../reusable-ui/Logo'
 import styled from 'styled-components'
 import { useParams , Link} from 'react-router-dom'
+import {theme} from '../../../theme/index'
+import { BsPersonCircle } from 'react-icons/bs'
 
 export default function Navbar() {
-const {username} = useParams()
+    const {username} = useParams();
+
   return (
-    <div>
-        <Logo />
+    <NavbarStyled>
+        <div className="nav-side-left">
+            <Logo />
+        </div>
 
         <div className="nav-side-right">
-            <h1>{`Hey, ${username}`}</h1>
-            <Link to={"/"}>
-            <button>Se déconnecter</button>
-            </Link>
+
+            <div className="nav-side-right-user">
+                <h1>{`Hey, ${username}`}</h1>
+
+                <Link to={"/"}>
+                <button >Se déconnecter</button>
+                </Link>
+            </div>
+
+            <BsPersonCircle className='icon'/>
+
         </div>
-    </div>
+    </NavbarStyled>
   )
 }
+
+const NavbarStyled = styled.div`
+    background-color: ${theme.colors.white};
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    height: 150px;
+    padding: 50px;
+`
