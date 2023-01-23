eaab50749e0fe93eff0d196394ae86825b090287
Cardona simon <simon.cdn@icloud.com>
2023-01-23 16:23:45 +0100

    ♻️ F04 : ORDERPAGE > refacto & add Main component

--- src/components/pages/order/OrderPage.js
+++ src/components/pages/order/OrderPage.js
@@ -2,40 +2,35 @@ import React from 'react'
 import Navbar from './Navbar'
 import styled from 'styled-components'
 import { theme } from '../../../theme'
+import Main from './Main'
 
 export default function OrderPage() {
   return (
-    <OrderPageStyle>
-      <div className="orderPage-container">
+    <OrderPageStyled>
+      <div className="container">
         <Navbar />
-        <main></main>
+        <Main />
       </div>
-    </OrderPageStyle>
+    </OrderPageStyled>
   )
 }
 
-const OrderPageStyle = styled.div`
+const OrderPageStyled = styled.div`
   background-color: ${theme.colors.primary};
   height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
   font-family: 'Open Sans', sans-serif;
-  .orderPage-container{
-    background-color: white;
+
+  .container{
+    background-color: ${theme.colors.white};
     width: 1400px;
     height: 90%;
     display: flex;
     flex-direction: column;
     justify-content: end;
-    border-radius: 15px;
+    border-radius: ${theme.borderRadius.extraRound};
     overflow: hidden;
-    main{
-      background-color: #F5F5F7;
-      width: 100%;
-      height: 90%;
-      flex-shrink: 1;
-      box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
-    }
   }
 `
