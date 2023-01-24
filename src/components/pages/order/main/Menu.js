37247a97853a966a64311eec770d630c2ee32189
Cardona simon <simon.cdn@icloud.com>
2023-01-24 16:09:56 +0100

    ✨ 💄 F05 : MENU > add style & data.map

--- src/components/pages/order/main/Menu.js
+++ src/components/pages/order/main/Menu.js
@@ -1,17 +1,36 @@
-import React from "react";
 import styled from "styled-components";
 import fakeData from "../../../../fakeData/fakeMenu";
 import { theme } from "../../../../theme";
+import Card from "./Card";
 
 export default function Menu() {
   console.log(fakeData);
-  return <MenuStyled>Menu</MenuStyled>;
+  return (
+    <MenuStyled>
+      {fakeData.map((item) => (
+        <Card
+          key={item.id}
+          title={item.title}
+          isAdvertised={item.isAdvertised}
+          isAvailable={item.isAvailable}
+          image={item.imageSource}
+          price={item.price}
+          quantity={item.quantity}
+        />
+      ))}
+    </MenuStyled>
+  );
 }
 
 const MenuStyled = styled.div`
-  height: 100%;
   width: 100%;
-  background-color: ${theme.colors.blue};
+  background-color: ${theme.colors.background_white};
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
   padding: 50px 50px 150px;
+  display: grid;
+  grid-template-columns: repeat(4, 240px);
+  grid-template-rows: repeat(3, 330px);
+  grid-row-gap: 60px;
+  grid-column-gap: 85px;
+  justify-content: center;
 `;
