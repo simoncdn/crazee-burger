deecf6b20e70b5d31d3e8daf17ff4f0c10bb996d
Cardona simon <simon.cdn@icloud.com>
2023-01-24 14:10:57 +0100

    🔥 F05 : delete basket file (flexbox fix)

--- src/components/pages/order/main/Main.js
+++ src/components/pages/order/main/Main.js
@@ -1,13 +1,11 @@
 import React from "react";
 import styled from "styled-components";
 import { theme } from "../../../../theme";
-import Basket from "./Basket";
 import Menu from "./Menu";
 
 export default function Main() {
   return (
     <MainStyled>
-      {/* <Basket /> */}
       <Menu />
     </MainStyled>
   );
