a1911da24ba59dbd2a0a05a9228f6933c9a6fabe
Cardona simon <simon.cdn@icloud.com>
2023-01-25 15:20:56 +0100

    💄 F05 : MENU > change styling

--- src/components/pages/order/main/Menu.js
+++ src/components/pages/order/main/Menu.js
@@ -1,20 +1,20 @@
+import { useState } from "react";
 import styled from "styled-components";
 import fakeData from "../../../../fakeData/fakeMenu";
 import { theme } from "../../../../theme";
+import { formatPrice } from "../../../../utils/maths";
 import Card from "./Card";
 
 export default function Menu() {
+  const [menu, setMenu] = useState(fakeData);
   return (
     <MenuStyled>
-      {fakeData.map((item) => (
+      {menu.map((product) => (
         <Card
-          key={item.id}
-          title={item.title}
-          isAdvertised={item.isAdvertised}
-          isAvailable={item.isAvailable}
-          image={item.imageSource}
-          price={item.price}
-          quantity={item.quantity}
+          key={product.id}
+          title={product.title}
+          image={product.imageSource}
+          leftDescription={formatPrice(product.price)}
         />
       ))}
     </MenuStyled>
@@ -22,27 +22,11 @@ export default function Menu() {
 }
 
 const MenuStyled = styled.div`
-  width: 100%;
   background-color: ${theme.colors.background_white};
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
   padding: 50px 50px 150px;
   display: grid;
-  grid-template-columns: repeat(4, 240px);
-  grid-template-rows: repeat(3, 330px);
+  grid-template-columns: repeat(4, 1fr);
   grid-row-gap: 60px;
-  grid-column-gap: 85px;
-  justify-content: center;
-  overflow: auto;
-  ::-webkit-scrollbar {
-    width: 8px;
-    background: transparent;
-  }
-  ::-webkit-scrollbar-thumb {
-    background: #666;
-    border-radius: 5px;
-  }
-  ::-webkit-scrollbar-button {
-    background: transparent;
-    height: 5px;
-  }
+  justify-items: center;
 `;
