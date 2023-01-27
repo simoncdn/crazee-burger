5eab734d55bd77a5548098383387ad72706e56d8
Cardona simon <simon.cdn@icloud.com>
2023-01-27 17:47:23 +0100

    ✨ F07 : MAIN > Add Panel component

--- src/components/pages/order/main/Main.js
+++ src/components/pages/order/main/Main.js
@@ -1,18 +1,41 @@
-import React from "react";
+import React, { useContext } from "react";
 import styled from "styled-components";
+import GlobalContext from "../../../context/GlobalContext";
+import Panel from "../panel/Panel";
 import Menu from "./Menu";
+import { theme } from "../../../../theme";
 
 export default function Main() {
+  const { windowPanel, isAdminMode } = useContext(GlobalContext);
+
   return (
     <MainStyled>
       <Menu />
+      <div className={isAdminMode ? "open" : "close"}>
+        <Panel className={windowPanel ? "max-height" : "min-height"} />
+      </div>
     </MainStyled>
   );
 }
 
 const MainStyled = styled.div`
   flex: 1;
+  background: ${theme.colors.background_white};
+  z-index: 100;
   display: grid;
   grid-template-columns: 1fr;
   overflow-y: scroll;
+  box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
+  .open {
+    display: block;
+  }
+  .close {
+    display: none;
+  }
+  .max-height {
+    height: 292px;
+  }
+  .min-height {
+    height: 42px;
+  }
 `;
