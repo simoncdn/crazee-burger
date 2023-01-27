374c2e8206e9246c3c877fd78594879c0a48b71a
Cardona simon <simon.cdn@icloud.com>
2023-01-27 17:50:44 +0100

    ✨ F07 : PRIMARYBUTTON > Add new props for reusability

--- src/components/reusable-ui/PrimaryButton.js
+++ src/components/reusable-ui/PrimaryButton.js
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 import { theme } from "../../theme";
 
-export default function PrimaryButton({ label, Icon, className }) {
+export default function PrimaryButton({ label, Icon, className, onClick }) {
   return (
-    <PrimaryButtonStyled className={className}>
+    <PrimaryButtonStyled className={className} onClick={onClick}>
       <span>{label}</span>
       {Icon && Icon}
     </PrimaryButtonStyled>
