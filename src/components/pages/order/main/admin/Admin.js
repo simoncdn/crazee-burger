92c53bc0214d1898234a2731c3c3e5a9481900be
Cardona simon <simon.cdn@icloud.com>
2023-03-14 18:01:45 +0100

    ✨ F07 : MAIN > add Admin component

---
+++ src/components/pages/order/main/admin/Admin.js
@@ -0,0 +1,23 @@
+import { useContext } from "react";
+import styled from "styled-components";
+import AdmninTabs from "./AdminTabs";
+import AdminPanel from "./AdminPanel";
+import GlobalContext from "../../../../../context/GlobalContext";
+
+export default function Admin({ className }) {
+  const { isCollapsed } = useContext(GlobalContext);
+
+  return (
+    <AdminStyled>
+      <AdmninTabs />
+      {!isCollapsed && <AdminPanel />}
+    </AdminStyled>
+  );
+}
+
+const AdminStyled = styled.div`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  left: 0;
+`;
