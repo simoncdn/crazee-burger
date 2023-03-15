c6f3f19d164909475a5c496a8456a1d19de76452
Cardona simon <simon.cdn@icloud.com>
2023-03-15 14:30:31 +0100

    ✨ F08 : MAIN > Add EmptyMenu component

---
+++ src/components/pages/order/main/EmptyMenu.js
@@ -0,0 +1,42 @@
+import React from "react";
+import styled from "styled-components";
+import { theme } from "../../../../theme";
+import PrimaryButton from "../../../reusable-ui/PrimaryButton";
+
+export default function EmptyMenu() {
+  return (
+    <EmptyMenuStyled>
+      <h3>Le menu est vide ?</h3>
+      <p>cliquez ci-dessous pour le réinitialiser</p>
+      <PrimaryButton
+        label="Générer de nouveaux produits"
+        classname="empty-button"
+      />
+    </EmptyMenuStyled>
+  );
+}
+
+const EmptyMenuStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  h3,
+  p {
+    text-transform: capitalize;
+    font-family: "Amatic SC";
+    font-size: ${theme.fonts.size.P4};
+    color: ${theme.colors.greyBlue};
+  }
+  h3 {
+    font-weight: ${theme.fonts.weights.bold};
+  }
+  p {
+    margin-top: 21px;
+    margin-bottom: 31px;
+  }
+  .empty-button {
+    width: fit-content;
+    font-size: ${theme.fonts.size.XS};
+  }
+`;
