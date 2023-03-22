49474728dab696a0b0f8848b0775bc5a801e1a7c
Cardona simon <simon.cdn@icloud.com>
2023-03-22 22:13:15 +0100

    💄 F08 : INDEX.HTML > HEAD > add favicon & title

--- src/components/pages/order/main/admin/adminPanel/addForm/AddForm.js
+++ src/components/pages/order/main/admin/adminPanel/addForm/AddForm.js
@@ -1,7 +1,6 @@
 import { useContext, useState } from "react";
 import styled from "styled-components";
 import GlobalContext from "../../../../../../../context/GlobalContext";
-import { theme } from "../../../../../../../theme";
 import ImagePreview from "./ImagePreview";
 import { getInputsConfig } from "./inputsConfig";
 import TextInput from "../../../../../../reusable-ui/TextInput";
