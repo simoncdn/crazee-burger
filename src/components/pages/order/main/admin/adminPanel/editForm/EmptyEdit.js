14989d18a411b23123288664085eb298ce1716cf
Cardona simon <simon.cdn@icloud.com>
2023-04-05 22:29:53 +0200

    💄 F09 : add EmptyEdit style

--- src/components/pages/order/main/admin/adminPanel/editForm/EmptyEdit.js
+++ src/components/pages/order/main/admin/adminPanel/editForm/EmptyEdit.js
@@ -1,11 +1,30 @@
 import React from "react";
 import { HiCursorClick } from "react-icons/hi";
+import styled from "styled-components";
 
 export default function EmptyEdit() {
   return (
-    <div>
+    <EmptyEditStyled>
       <span>Cliquer sur un produit pour le modifier</span>
-      <HiCursorClick />
-    </div>
+      <HiCursorClick className="icon" />
+    </EmptyEditStyled>
   );
 }
+
+const EmptyEditStyled = styled.div`
+  display: flex;
+  gap: 11px;
+  align-items: center;
+  margin-top: 50px;
+  span {
+    text-transform: uppercase;
+    font-family: "Amatic SC";
+    font-size: 24px;
+    color: #747b91;
+  }
+  .icon {
+    width: 20px;
+    height: 20px;
+    color: #747b91;
+  }
+`;
