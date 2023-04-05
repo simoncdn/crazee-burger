2bc63f48ba877f5e4c2d39d6d40473f4b8d82240
Cardona simon <simon.cdn@icloud.com>
2023-04-05 16:45:02 +0200

    ✨ F09 : EDIT > Add EmptyEdit

---
+++ src/components/pages/order/main/admin/adminPanel/editForm/EmptyEdit.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { HiCursorClick } from "react-icons/hi";
+
+export default function EmptyEdit() {
+  return (
+    <div>
+      <span>Cliquer sur un produit pour le modifier</span>
+      <HiCursorClick />
+    </div>
+  );
+}
