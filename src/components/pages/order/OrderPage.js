5c5b15ea22d8f211b6007b95f6f1926000949943
Cardona simon <simon.cdn@icloud.com>
2023-04-19 15:58:17 +0200

    ✨ F12 : ORDER > Connect basket product to product to Edit

--- src/components/pages/order/OrderPage.js
+++ src/components/pages/order/OrderPage.js
@@ -8,9 +8,10 @@ import { fakeMenu } from "../../../fakeData/fakeMenu";
 import { EMPTY_PRODUCT } from "../../../enum/product";
 import { deepClone } from "../../../utils/deepClone";
 import { focusOnRef } from "../../../utils/focusOnRef";
+import { replaceFrenchCommaWithDot } from "../../../utils/maths";
 
 export default function OrderPage() {
-  const [isAdminMode, setIsAdminMode] = useState(false);
+  const [isAdminMode, setIsAdminMode] = useState(true);
   const [isCollapsed, setIsCollapsed] = useState(false);
   const [currentTabSelected, setCurrentTabSelected] = useState("add");
   const [menu, setMenu] = useState(fakeMenu.LARGE);
@@ -46,11 +47,24 @@ export default function OrderPage() {
 
   const handleEdit = (productToEdit) => {
     const menuCopy = deepClone(menu);
+    const basketMenuCopy = deepClone(basketMenu);
     const productTobeEdited = menuCopy.findIndex(
       (product) => product.id === productToEdit.id
     );
     menuCopy[productTobeEdited] = productToEdit;
+
+    const productInBasket = basketMenuCopy.findIndex(
+      (product) => product.id === productToEdit.id
+    );
+
+    //@Find a better implementation
+    basketMenuCopy[productInBasket].title = productToEdit.title;
+    basketMenuCopy[productInBasket].imageSource = productToEdit.imageSource;
+    basketMenuCopy[productInBasket].price = productToEdit.price;
+
     setMenu(menuCopy);
+    setBasketMenu(basketMenuCopy);
+    newPrice(productToEdit);
   };
 
   const resetMenu = () => {
@@ -58,14 +72,21 @@ export default function OrderPage() {
   };
 
   // CRUD BASKET //
+
   const addProductToBasket = (idProductToAdd) => {
     const basketCopy = deepClone(basketMenu);
-
+    const menuCopy = deepClone(menu);
     const productToAdd = menu.find((product) => product.id === idProductToAdd);
-    productToAdd.price = parseInt(productToAdd.price);
+    // productToAdd.price = parseInt(productToAdd.price);
     productToAdd.quantity = 1;
     const basketUpdated = [productToAdd, ...basketCopy];
 
+    const productFound = menuCopy.findIndex(
+      (product) => product.id === idProductToAdd
+    );
+    menuCopy[productFound].quantity += 1;
+
+    setMenu(menuCopy);
     setBasketMenu(basketUpdated);
     getPrice(productToAdd.price);
 
@@ -97,17 +118,35 @@ export default function OrderPage() {
 
   const getPrice = (productPrice) => {
     let newTotalPrice = totalPrice;
-    newTotalPrice += productPrice;
+    newTotalPrice += replaceFrenchCommaWithDot(productPrice);
     setTotalPrice(newTotalPrice);
   };
 
   const updatePrice = (productId) => {
     const totalPriceCopy = totalPrice;
     const findProduct = basketMenu.find((product) => product.id === productId);
-
+    console.log(findProduct);
     const newTotalPrice =
       totalPriceCopy - findProduct.quantity * findProduct.price;
     setTotalPrice(newTotalPrice);
+  };
+
+  const newPrice = (productToEdit) => {
+    const priceToProduct = productToEdit.price;
+    const basketMenuCopy = deepClone(basketMenu);
+    const productInBasket = basketMenuCopy.findIndex(
+      (product) => product.id === productToEdit.id
+    );
+    basketMenuCopy[productInBasket] = productToEdit;
+    basketMenuCopy[productInBasket].price = priceToProduct;
+
+    let newTotal = 0;
+    basketMenuCopy.forEach((product) => {
+      if (!product.price) return;
+      // console.log(replaceFrenchCommaWithDot(product.price));
+      newTotal += replaceFrenchCommaWithDot(product.price) * product.quantity;
+    });
+    setTotalPrice(newTotal);
   };
 
   const globalContextValue = {
