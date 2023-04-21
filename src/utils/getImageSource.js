b35ce9d1962b8f7001c1bef55744c2a9539e20f3
Cardona simon <simon.cdn@icloud.com>
2023-04-21 17:47:09 +0200

    🚚 F12 : UTILS > getImageSource > change Path

--- src/utils/getImageSource.js
+++ src/utils/getImageSource.js
@@ -1,4 +1,4 @@
-import { IMAGE_DEFAULT } from "../../../../../../enum/imageDefault";
+import { IMAGE_DEFAULT } from "../enum/imageDefault";
 
 export const getImageSource = (imageSource) => {
   return imageSource || IMAGE_DEFAULT;
