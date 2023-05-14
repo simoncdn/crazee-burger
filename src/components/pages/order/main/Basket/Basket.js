6b2babe39acd71b810d87b1fd9498fcedfb43710
Cardona simon <simon.cdn@icloud.com>
2023-05-14 16:48:16 +0200

    ♻️ F12 > BASKET > Refacto

--- src/components/pages/order/main/Basket/Basket.js
+++ src/components/pages/order/main/Basket/Basket.js
@@ -1,36 +1,18 @@
-import React, { useContext } from "react";
+import React from "react";
 import styled from "styled-components";
 import { theme } from "../../../../../theme";
-import GlobalContext from "../../../../../context/GlobalContext";
 import BasketBody from "./BasketBody";
-import {
-  formatPrice,
-  replaceFrenchCommaWithDot,
-} from "../../../../../utils/maths";
+import Footer from "./Footer";
+import Total from "./Total";
 
 export default function Basket() {
-  const { basketMenu } = useContext(GlobalContext);
-
-  const updateTotalPrice = basketMenu.reduce((total, product) => {
-    const price = replaceFrenchCommaWithDot(product.price);
-
-    if (isNaN(price)) return total;
-    total += price * product.quantity;
-    return total;
-  }, 0);
-
   return (
     <BasketStyled>
-      <div className="total">
-        Total
-        <span>{formatPrice(updateTotalPrice)}</span>
-      </div>
+      <Total />
 
       <BasketBody />
 
-      <div className="footer">
-        <span>Codé avec ❤️ et React.js</span>
-      </div>
+      <Footer />
     </BasketStyled>
   );
 }
@@ -42,32 +24,4 @@ const BasketStyled = styled.div`
   box-shadow: 8px 0px 20px 8px rgba(0, 0, 0, 0.2) inset; // alternative plus proche de la maquette
   background-color: ${theme.colors.background_white};
   overflow-y: hidden;
-
-  .total,
-  .footer {
-    height: 70px;
-    background-color: ${theme.colors.background_dark};
-  }
-
-  .total {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 12px 18px;
-    font-family: ${theme.fonts.family.stylish};
-    color: ${theme.colors.primary};
-    font-size: ${theme.fonts.size.P4};
-    span {
-      font-weight: ${theme.fonts.weights.bold};
-    }
-  }
-
-  .footer {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    color: ${theme.colors.white};
-    font-family: ${theme.fonts.family.stylish};
-    font-size: ${theme.fonts.size.P2};
-  }
 `;
