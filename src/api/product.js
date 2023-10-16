c6a3ceef1643e459ad63a0610a9ca41c10763240
sdn <cdna.simon@gmail.com>
2023-10-16 21:18:38 +0200

    ✨ F13 : API > Add product CRud

--- src/api/product.js
+++ src/api/product.js
@@ -1,24 +1,21 @@
-import { doc, getDoc, setDoc } from "firebase/firestore"
+import { doc, getDoc, setDoc } from "firebase/firestore";
 import { db } from "./api";
 
 export const getMenu = async (userId) => {
-    const docRef = doc(db, "users", userId);
-    const docSnap = await getDoc(docRef);
+  const docRef = doc(db, "users", userId);
+  const docSnap = await getDoc(docRef);
 
-    if (docSnap.exists()) {
-        const userReceived = docSnap.data();
-        return userReceived.menu;
-    }
-    else{
-        return null;
-    }
+  if (docSnap.exists()) {
+    const userReceived = docSnap.data();
+    return userReceived.menu;
+  }
 };
 
 export const syncBothMenu = (userId, menu) => {
-    const docRef = doc(db, "users", userId);
-    const user = {
-        username: userId,
-        menu: menu
-    }
-    setDoc(docRef, user);
-}
+  const docRef = doc(db, "users", userId);
+  const user = {
+    username: userId,
+    menu: menu,
+  };
+  setDoc(docRef, user);
+};
