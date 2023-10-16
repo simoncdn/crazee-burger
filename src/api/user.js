a5dfe84ce3cf9f1174e1ed8e7d4fe12150791fa8
sdn <cdna.simon@gmail.com>
2023-10-16 21:18:17 +0200

    ✨ F13 : API > Add user CRud

--- src/api/user.js
+++ src/api/user.js
@@ -1,32 +1,27 @@
-import {  getDoc, setDoc, doc } from "firebase/firestore";
+import { getDoc, setDoc, doc } from "firebase/firestore";
 import { db } from "./api";
 import { fakeMenu } from "../fakeData/fakeMenu";
 
 export const getUser = async (userId) => {
-    const docRef = doc(db, "users", userId);
-    const docSnap = await getDoc(docRef);
+  const docRef = doc(db, "users", userId);
+  const docSnap = await getDoc(docRef);
 
-    if (docSnap.exists()) {
-        const userReceived = docSnap.data();
-        return userReceived;
-    }
-    else{
-        return null;
-    }
+  if (docSnap.exists()) {
+    const userReceived = docSnap.data();
+    return userReceived;
+  }
 };
-export const addUser = async (userId) => {
-    const docRef = doc(db, "users", userId);
-    const user = {
-        username: userId,
-        menu: fakeMenu.LARGE
-    }
-
-    await setDoc(docRef, user);
-} 
-export const authenticateUser = async(userId) => {
-    const existingUser = await getUser(userId);
-    if (!existingUser) {
-        return addUser(userId);
-    }
-    return existingUser;
-}
+export const addUser = (userId) => {
+  const docRef = doc(db, "users", userId);
+  const user = {
+    username: userId,
+    menu: fakeMenu.LARGE,
+  };
+  setDoc(docRef, user);  
+};
+export const authenticateUser = async (userId) => {
+  const existingUser = await getUser(userId);
+  if (!existingUser) {
+    addUser(userId);
+  }
+};
