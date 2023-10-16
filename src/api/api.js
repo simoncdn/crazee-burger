36fc33f8c5991135f2057fa13c4849cce7a0ba11
sdn <cdna.simon@gmail.com>
2023-10-16 21:17:44 +0200

    6✨ F13 : API > Add api config

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -14,6 +10,5 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_APP_ID 
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
